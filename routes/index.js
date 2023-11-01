10c9b14417d5ef2bb690aae13c90fe24c3bf8c24
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-01 23:39:42 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,3 @@
-/*
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.html');
-});
-
-module.exports = router;
-*/
-
 
 const express = require('express');
 const bodyParser = require('body-parser');
