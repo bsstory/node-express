1972152d1b3e67239a0dbf1f3b59149e6299ac5e
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-02 00:00:27 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+/* GET test page. */
+router.get('/test', function(req, res, next) {
+  res.send('test');
+});
+
+
 module.exports = router;
