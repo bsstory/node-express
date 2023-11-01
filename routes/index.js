5f90b733c2fd16f196f7c160ae419b8f443a2210
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-01 23:56:06 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
 
-app.use(bodyParser.json());
-
-app.post('/google-chat-webhook', (req, res) => {
-  // Google Chat에서 메시지 이벤트를 받음
-  const message = req.body.message.text;
-
-  // 답장 메시지 생성
-  const reply = {
-    text: `You said: ${message}`
-  };
-
-  // 답장 메시지를 Google Chat으로 보냄
-  res.json(reply);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.get('/', (req, res) => {
+  res.send('test');
 });
