3377715cc4f35178dd9898f6b474728e6cc6541e
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-02 13:39:33 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,69 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var router = express.Router();
-
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const {google} = require('googleapis');
+const router = express.Router();
 
 router.use(bodyParser.json());
 
+const calendar = google.calendar('v3');
+const oauth2Client = new google.auth.OAuth2(
+  'YOUR_CLIENT_ID',
+  'YOUR_CLIENT_SECRET',
+  'YOUR_REDIRECT_URI'
+);
+
+// TODO: 토큰을 얻기 위해 OAuth2 인증 흐름을 완료하세요.
+// oauth2Client.setCredentials({access_token: 'YOUR_ACCESS_TOKEN', refresh_token: 'YOUR_REFRESH_TOKEN'});
 
 /* Respond to Google Chat messages */
-router.post('/chat', function(req, res, next) {
-  const message = req.body.message.text;
-  const reply = {
-    text: message,
-  };
-  res.json(reply);
+router.post('/chat', async function(req, res, next) {
+  const messageText = req.body.message.text;
+
+  if (messageText.startsWith('/캘린더 ')) {
+    const calendarName = messageText.replace('/캘린더 ', '').trim();
+    try {
+      const calendarList = await calendar.calendarList.list({
+        auth: oauth2Client,
+      });
+
+      const calendarId = calendarList.data.items.find(cal => cal.summary === calendarName)?.id;
+
+      if (!calendarId) {
+        return res.json({text: '캘린더를 찾을 수 없습니다.'});
+      }
+
+      const now = new Date();
+      const startOfDay = new Date(now);
+      startOfDay.setHours(0, 0, 0, 0);
+
+      const endOfDay = new Date(startOfDay);
+      endOfDay.setDate(endOfDay.getDate() + 1);
+
+      const events = await calendar.events.list({
+        auth: oauth2Client,
+        calendarId,
+        timeMin: startOfDay.toISOString(),
+        timeMax: endOfDay.toISOString(),
+        singleEvents: true,
+        orderBy: 'startTime',
+      });
+
+      const eventTexts = events.data.items.map(event => {
+        const start = event.start.dateTime || event.start.date;
+        return `${start}: ${event.summary}`;
+      }).join('\n');
+
+      const replyText = eventTexts || '오늘 일정이 없습니다.';
+      res.json({text: replyText});
+    } catch (error) {
+      console.error('Google Calendar API Error:', error);
+      res.status(500).json({text: '일정을 불러오는 중 에러가 발생했습니다.'});
+    }
+  } else {
+    // 기본적으로 메시지를 그대로 반환
+    const reply = {text: messageText};
+    res.json(reply);
+  }
 });
 
 /* GET home page. */
@@ -25,6 +75,5 @@ router.get('/', function(req, res, next) {
 router.get('/test', function(req, res, next) {
   res.send('test');
 });
-
 
 module.exports = router;
