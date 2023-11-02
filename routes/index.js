01f6ccba51f2fe616e2da3649f8be3ebc79deb85
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-02 13:32:15 +0900

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+
+
+
+router.use(bodyParser.json());
+
+
+/* Respond to Google Chat messages */
+router.post('/chat', function(req, res, next) {
+  const message = req.body.message.text;
+  const reply = {
+    text: message,
+  };
+  res.json(reply);
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index.html');
+});
+
+/* GET test page. */
+router.get('/test', function(req, res, next) {
+  res.send('test');
+});
+
+
+module.exports = router;
