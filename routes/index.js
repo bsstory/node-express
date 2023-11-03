9e34e281ad9c8b804dba38ee3d8e33c7e2715a12
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-03 15:52:54 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
 var router = express.Router();
 router.use(bodyParser.json());
+
+
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
