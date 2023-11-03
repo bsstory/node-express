cefaff735eb0983c3940eeab292118c96202a600
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-03 16:08:37 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -55,6 +55,29 @@ async function authorize() {
   return client;
 }
 
+async function listEvents(auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+  const res = await calendar.events.list({
+    calendarId: 'primary',
+    timeMin: new Date().toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  });
+  const events = res.data.items;
+  if (!events || events.length === 0) {
+    console.log('No upcoming events found.');
+    return;
+  }
+  console.log('Upcoming 10 events:');
+  events.map((event, i) => {
+    const start = event.start.dateTime || event.start.date;
+    console.log(`${start} - ${event.summary}`);
+  });
+}
+
+authorize().then(listEvents).catch(console.error);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
