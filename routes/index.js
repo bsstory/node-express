14c8f6d60ac7ffbb873727bfb3bfb1cd866f7980
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-03 17:49:59 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -90,4 +90,14 @@ function listEvents(auth) {
     }
   });
 }
+app.get('/auth', async (req, res) => {
+  try {
+    const auth = await authenticate();
+    const events = await listEvents(auth);
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 module.exports = router;
