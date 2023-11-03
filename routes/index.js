7fd9b53a8cbcbe03491583d8afbeb03b3f6bf989
KIM SEUNGWOO <77507261+bsstory@users.noreply.github.com>
2023-11-03 15:58:04 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,20 @@ async function loadSavedCredentialsIfExist() {
   }
 }
 
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+
 
 
 /* GET home page. */
